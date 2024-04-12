278fdf78a17820f59004acf9b23f25dae12ef13d
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-11 21:29:09 -0400

    push method

--- linked-list.js
+++ linked-list.js
@@ -26,7 +26,12 @@ class LinkedList {
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+
+    this.length += 1;
   }
 
   /** unshift(val): add new value to start of list. */
